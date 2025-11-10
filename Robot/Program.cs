96a3e2b75aae8fe71898faa9cc48767bae97594a
TacoConKvass <e_frun@o2.pl>
2025-11-10 14:55:26 +0100

    Add: CH3, CH6a

---
+++ Robot/Program.cs
@@ -0,0 +1,93 @@
+﻿using Bot = CieploZimnoBib.CieploZimno;
+using Console = System.Console;
+using CommandMap = System.Collections.Generic.Dictionary<char, Robot.Direction>;
+using Convert = System.Convert;
+
+namespace Robot
+{
+    enum Direction
+    {
+        Forward = 0b00,
+        Left = 0b01,
+        Right = 0b10,
+        Back = 0b11
+    }
+
+    public static class Program
+    {
+        static readonly CommandMap commands = new CommandMap() {
+            { 'f', Direction.Forward },
+            { 'b', Direction.Back },
+            { 'l', Direction.Left },
+            { 'r', Direction.Right },
+            { 'F', Direction.Forward },
+            { 'B', Direction.Back },
+            { 'L', Direction.Left },
+            { 'R', Direction.Right },
+        };
+
+        public static void Main(string[] args)
+        {
+            byte distance = 63;
+            short battery = 150;
+            while (distance != 0 && battery != 0)
+            {
+                short command = 0;
+                Log($"\nBattery: {battery}\tDistance: {distance}");
+
+                var direction = AskDirection();
+                command = (short)((ushort)command | (byte)direction);
+
+                var move_distance = AskDistance();
+                command = (short)((ushort)command | (move_distance << 2));
+
+                short answer = Bot.Poszukiwanie(command);
+                battery = (short)((short)(answer << 7) >> 7);
+                distance = (byte)(answer >> 9);
+            }
+
+            if (distance == 0) Log($"\nTarget reached! Battery left: {battery}");
+            else Log($"\nRan out of battery {distance} units from the target!");
+        }
+
+        static Direction AskDirection()
+        {
+            Log("Rotate: [F]orward, [B]ack, [L]eft, [R]ight");
+            char direction = ' ';
+            while (direction == ' ')
+            {
+                direction = Console.ReadKey().KeyChar;
+                if (commands.TryGetValue(direction, out Direction move_direction))
+                {
+                    Log($"\r{move_direction}\n");
+                    return move_direction;
+                }
+
+                Log($"\rSupplied direction is not valid {direction}\r");
+                direction = ' ';
+            }
+            return Direction.Forward;
+        }
+
+        static byte AskDistance()
+        {
+            Log("Choose distance: 1-32");
+            sbyte move_distance;
+            string read = "";
+            while (true)
+            {
+                try
+                {
+                    read = Console.ReadLine();
+                    move_distance = (sbyte)(Convert.ToSByte(read) - 1);
+                    if (move_distance >= 0 && move_distance <= 31) break;
+                }
+                catch { };
+                Log($"Distance {read} is invalid");
+            }
+            return (byte)move_distance;
+        }
+
+        static void Log(object o) => Console.WriteLine(o);
+    }
+}
---
+++ Szyfry/Program.cs
@@ -0,0 +1,144 @@
+﻿using Console = System.Console;
+using Enum = System.Enum;
+using Reader = System.IO.StreamReader;
+using File = System.IO.File;
+using CharToIndexMap = System.Collections.Generic.Dictionary<char, int>;
+
+namespace Szyfry
+{
+    public static class Program
+    {
+        public enum Mode
+        {
+            encode,
+            decode,
+        }
+
+        public class Args
+        {
+            public int shift;
+            public string input_file_name;
+            public Mode mode;
+            public string alfabet;
+            public CharToIndexMap char_to_index;
+        }
+
+        public static void Main(string[] args) {
+            var opts = ParseArgs(args);
+            if (opts == null) return;
+
+            using (var input_file = new Reader(opts.input_file_name))
+            {
+
+                switch (opts?.mode)
+                {
+                    case Mode.encode:
+                        Encode(input_file, opts.shift, opts.alfabet, opts.char_to_index);
+                        break;
+                    case Mode.decode:
+                        Decode(input_file, opts.shift, opts.alfabet, opts.char_to_index);
+                        break;
+                }
+            }
+        }
+
+        public static void Encode(Reader file, int shift, string alfabet, CharToIndexMap index_map) {
+            while (!file.EndOfStream)
+            {
+                string line = file.ReadLine();
+                foreach (char c in line)
+                {
+                    if (!ShiftIndex(c, alfabet, index_map, shift, out int index))
+                    {
+                        Console.Write(c);
+                        continue;
+                    }
+
+                    char output = alfabet[index];
+                    if (char.IsUpper(c))
+                    {
+                        output = char.ToUpper(output);
+                    }
+
+                    Console.Write(output);
+                }
+                Console.Write('\n');
+            }
+        }
+
+        public static void Decode(Reader file, int shift, string alfabet, CharToIndexMap index_map) {
+            while (!file.EndOfStream) {
+                string line = file.ReadLine();
+                foreach (char c in line) {
+                    if (!ShiftIndex(c, alfabet, index_map, -shift, out int index))
+                    {
+                        Console.Write(c);
+                        continue;
+                    }
+
+                    char output = alfabet[index];
+                    if (char.IsUpper(c)) {
+                        output = char.ToUpper(output);
+                    }
+
+                    Console.Write(output);
+                }
+                Console.Write('\n');
+            }
+        }
+
+        public static bool ShiftIndex(char c, string alfabet, CharToIndexMap index_map, int shift, out int index) {
+            if (!index_map.TryGetValue(char.ToLower(c), out index)) return false;
+
+            index += shift;
+            if (index < 0) index += alfabet.Length;
+            if (index >= alfabet.Length) index -= alfabet.Length;
+            return true;
+        }
+
+        public static Args ParseArgs(string[] args) {
+            if (args.Length < 3) {
+                ShowUsage();
+                return null;
+            }
+            
+            if (!Enum.TryParse(args[0], ignoreCase: true, out Mode mode)) {
+                Console.WriteLine($"Unknown mode: {args[0]}");
+                ShowUsage();
+                return null;
+            }
+
+            var input_file_name = args[1];
+            if (!File.Exists(input_file_name)) {
+                Console.WriteLine($"File {input_file_name} doesn't exist");
+                ShowUsage();
+                return null;
+            }
+
+            if (!int.TryParse(args[2], out int shift)) {
+                Console.WriteLine($"Shift \"{args[2]}\" is not a number");
+                ShowUsage();
+            }
+            
+            if (!File.Exists("alfabet.txt")) {
+                Console.WriteLine($"File alfabet.txt doesn't exist. Please create one");
+                return null;
+            }
+            
+            string alfabet = "";
+            using (var reader = new Reader("alfabet.txt")) {
+                while (!reader.EndOfStream)
+                    alfabet += reader.ReadLine();
+            }
+
+            var char_to_index = new CharToIndexMap();
+            for (int i = 0; i < alfabet.Length; i++) {
+                char_to_index.Add(char.ToLower(alfabet[i]), i);
+            }
+            
+            return new Args { shift = shift % alfabet.Length, mode = mode, input_file_name = input_file_name, alfabet = alfabet, char_to_index = char_to_index };
+        }
+
+        public static void ShowUsage() => Console.WriteLine("\nUsage: dotnet Szyfry.dll (encode|decode) filename shift");
+    }
+}
